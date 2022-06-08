5fb13e1377fe3b9506b3ac659537bbcce40cb144
va yt <vayt@wings.cage>
2022-06-08 10:10:24 +0430

    Adding documents, Improve logic design

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,52 @@
+#![crate_name = "tbge"]
+ //! This carte is a turn-base game engine in Rust. 
 use std::fmt::{Debug, Display};
 
+/// The Act trait marks a type as a game act
+pub trait Act: Display + Debug + Send + Sync {}
 
-pub trait Act: Display + Debug + Send + Sync {
-    fn affect(self, status:&mut dyn Status) -> &mut dyn Status;
+/// The State trait defines the behavior of game states
+pub trait State {
+    /// determine the required act (as trait object) that alter the current state to given state
+    fn diff(&self, other: &Self) -> dyn Act
+    where
+        Self: Sized;
+    
+    /// update the current state according to given act
+    fn update(&mut self, act: &dyn Act);
 }
 
-pub trait Player: Display + Debug + Send + Sync {
-    fn name(&self) -> String;
-    fn acts(&self) -> dyn Iterator<Item = dyn Act>;
-    fn play(&self, status: &dyn Status) -> dyn Act;
-}
+/// The Move trait marks an iterator of act as some sequential acts of player in each turn
+pub trait Move: Display + Debug + Send + Sync + Iterator<Item = dyn Act> {}
 
-pub trait Status: Display + Debug + Send + Sync {
-    fn score(&self, player: dyn Player) -> String;
+/// Defines the behavior of overall game situation as an iterator of some states
+/// for example in Poker states can be: each player cards, revealed cards, unrevealed cards, current bet, ...
+// Todo: must consider public and private states. [this may be avoided with not providing the whole game status to players]
+pub trait Status: Display + Debug + Send + Sync + Iterator<Item = dyn State> {
+    /// tells the score of specified player in current situation
+    fn score(&self, player: dyn Player) -> dyn ToString;
+    /// specifies which player's turn is.
     fn turn(&self) -> dyn Player;
-    fn previlages(
-        &self,
-        acts: dyn Iterator<Item = dyn Act>,
-        player: dyn Player,
-    ) -> dyn Iterator<Item = dyn Act>;
-} 
+    /// validates the given move due the game status returns true if the move is valid
+    fn validate(&self, mov: dyn Move) -> bool;
+}
+
+/// This trait defines behavior of a player
+/// Players must be able to think and move according to game status
+pub trait Player: Display + Debug + Send + Sync {
+    /// Tells the player name. We do not play with strangers
+    fn name(&self) -> String;
+    /// The thinking function to decide the move with given game status
+    fn play(&self, status: &dyn Status) -> dyn Move;
+}
 
 
-pub trait Game: Iterator<Item = dyn Status>{
+/// Defines behavior of a turn-based game
+/// Game iterates over sequence of situations and ends after the iteration
+pub trait Game: Iterator<Item = dyn Status> {
+    /// Specifies the players
     fn players(&self) -> dyn Iterator<Item = dyn Player>;
+    /// Specifies the current situation of game
     fn status(&self) -> dyn Status;
 }
+
